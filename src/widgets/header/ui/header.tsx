3f7ecba1ee0c35091d1f3e5b14e815a32f28120e
Evgenii Ponomarenko <ponev86@gmail.com>
2024-12-09 21:59:28 +0800

    fix header

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,6 +1,6 @@
 import { FC, memo } from 'react';
 
-import styles from './Header.module.scss';
+import styles from './header.module.scss';
 
 export const Header: FC = memo(() => {
   return <header className={styles.header}>NOTES APP</header>;
